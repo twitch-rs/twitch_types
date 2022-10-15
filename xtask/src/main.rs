83488ba03d3987564d587ae4a9317e22267382dc
Emil Gardström <emil.gardstrom@gmail.com>
2022-10-15 12:29:53 +0200

    add and remove release label

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -36,6 +36,9 @@ fn main() -> color_eyre::Result<()> {
                     "gh repo view --json defaultBranchRef --jq .defaultBranchRef.name"
                 )
                 .read()?;
+                if !cmd!(sh, "gh pr view --json labels").read().unwrap_or_default().contains("release") {
+                    cmd!(sh, "gh pr edit --add-label release").run()?;
+                }
                 let dry_run = sh.var("CI").is_err() || current_branch != default_branch;
                 eprintln!("Taging!{}!", if dry_run { " (dry run)" } else { "" });
 
@@ -62,6 +65,9 @@ fn main() -> color_eyre::Result<()> {
                     cmd!(sh, "git push origin {tag}").run()?;
                 }
             } else {
+                if cmd!(sh, "gh pr view --json labels").read().unwrap_or_default().contains("release") {
+                    cmd!(sh, "gh pr edit --remove-label release").run()?;
+                }
                 eprintln!("tag exists already, no action needed");
             }
         }
