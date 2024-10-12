babdf49f652544317394ec19403160aaa60915dc
Nerixyz <nerixdev@outlook.de>
2024-10-12 23:26:21 +0200

    feat: add whisper ID

--- src/chat.rs
+++ src/chat.rs
@@ -4,3 +4,10 @@ manual_braid! {
     pub struct SharedChatSessionIdRef;
 }
 impl_extra!(SharedChatSessionId, SharedChatSessionIdRef);
+
+manual_braid! {
+    /// A Whisper ID
+    pub struct WhisperId;
+    pub struct WhisperIdRef;
+}
+impl_extra!(WhisperId, WhisperIdRef);
