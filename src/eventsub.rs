ac49f4bb9f858c236d64f03f28deedaf437709f4
Nerixyz <nerixdev@outlook.de>
2024-10-12 22:45:04 +0200

    feat: add conduit types

--- src/eventsub.rs
+++ src/eventsub.rs
@@ -4,3 +4,17 @@ manual_braid! {
     pub struct EventSubIdRef;
 }
 impl_extra!(EventSubId, EventSubIdRef);
+
+manual_braid! {
+    /// An ID of a Conduit
+    pub struct ConduitId;
+    pub struct ConduitIdRef;
+}
+impl_extra!(ConduitId, ConduitIdRef);
+
+manual_braid! {
+    /// An ID of a Conduit Shard
+    pub struct ConduitShardId;
+    pub struct ConduitShardIdRef;
+}
+impl_extra!(ConduitShardId, ConduitShardIdRef);
