1367dee4a7c9ffa4e4ed0a935a98fb6ac8ea992d
Faey <1144986+FaeyUmbrea@users.noreply.github.com>
2025-10-07 18:25:36 +0200

    Update extension.rs

--- src/extension.rs
+++ src/extension.rs
@@ -15,28 +15,6 @@ impl_extra!(ExtensionClientId, ExtensionClientIdRef);
 manual_braid!{
     /// A Bits Transaction ID
     pub struct BitsTransactionId;
-    pub struct BitsTransactionIdref;
+    pub struct BitsTransactionIdRef;
 }
-impl_extra!(BitsTransactionId, BitsTransactionIdref);
-
-/// An extension product
-#[derive(Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(
-    feature = "serde",
-    derive(serde_derive::Serialize, serde_derive::Deserialize)
-)]
-#[cfg_attr(feature = "deny_unknown_fields", serde(deny_unknown_fields))]
-#[non_exhaustive]
-pub struct ExtensionProduct {
-    /// The Display name of the purchased product
-    pub name: String,
-
-    /// The sku of the purchased product
-    pub sku: String,
-
-    /// The amount of bits paid for the product
-    pub bits: i64,
-
-    /// If the product is in development (bits will always be 0 if this is true)
-    pub in_development: bool,
-}
+impl_extra!(BitsTransactionId, BitsTransactionIdRef);
